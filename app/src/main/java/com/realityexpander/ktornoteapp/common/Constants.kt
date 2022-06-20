8e50fff0661af39df48ff805dacee1faec102bf4
Chris Athanas <realityexpander@gmail.com>
2022-06-20 02:30:08 -0500

    Add auth interceptor

---
+++ app/src/main/java/com/realityexpander/ktornoteapp/common/Constants.kt
@@ -0,0 +1,11 @@
+package com.realityexpander.ktornoteapp.common
+
+object Constants {
+
+    // These endpoints will be accessed without authentication headers
+    val PUBLIC_ENDPOINTS = listOf(
+        "/login",
+        "/register"
+    )
+
+}
---
+++ app/src/main/java/com/realityexpander/ktornoteapp/data/remote/BasicAuthInterceptor.kt
@@ -0,0 +1,26 @@
+package com.realityexpander.ktornoteapp.data.remote
+
+import com.realityexpander.ktornoteapp.common.Constants.PUBLIC_ENDPOINTS
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class BasicAuthInterceptor: Interceptor {
+    var email: String? = null
+    var password: String? = null
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val request = chain.request()
+
+        // Check for publicly-accessible API endpoints
+        if(request.url.encodedPath in PUBLIC_ENDPOINTS) {
+            return chain.proceed(request)
+        }
+
+        val authenticatedRequest = request.newBuilder()
+            .header("Authorization", Credentials.basic(email ?: "", password ?: ""))
+            .build()
+        return chain.proceed(authenticatedRequest)
+    }
+}
