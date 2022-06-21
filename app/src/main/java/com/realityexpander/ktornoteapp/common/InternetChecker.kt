d430e6991410c1f5a77e53302d5ec3102bffa8a7
Chris Athanas <realityexpander@gmail.com>
2022-06-20 21:15:05 -0500

    Add InternetChecker.kt

---
+++ app/src/main/java/com/realityexpander/ktornoteapp/common/InternetChecker.kt
@@ -0,0 +1,32 @@
+package com.realityexpander.ktornoteapp.common
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+fun checkForInternetConnection(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
+            else -> false
+        }
+    }
+
+    // Below Android M
+    val activeNetworkInfo = connectivityManager.activeNetworkInfo ?: return false
+    return (
+            activeNetworkInfo.isAvailable ||
+            activeNetworkInfo.isConnected ||
+            activeNetworkInfo.isConnectedOrConnecting
+    )
+}
