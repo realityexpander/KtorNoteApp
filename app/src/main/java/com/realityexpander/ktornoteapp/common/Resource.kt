3320e6bb93e8eea0c4396fb5bf7015552a5a6ed0
Chris Athanas <realityexpander@gmail.com>
2022-06-20 20:59:19 -0500

    Add Event and Resource classes

---
+++ app/src/main/java/com/realityexpander/ktornoteapp/common/Resource.kt
@@ -0,0 +1,29 @@
+package com.realityexpander.ktornoteapp.common
+
+data class Resource<out T>(
+    val status: Status,
+    val data: T?,
+    val message: String?
+) {
+
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(Status.SUCCESS, data, null)
+        }
+
+        fun <T> error(message: String, data: T?): Resource<T> {
+            return Resource(Status.ERROR, data, message)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(Status.LOADING, data, null)
+        }
+    }
+
+}
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
